60ac786c8acb32d19212bf302174da013417136d
Alouiscious Media <info@reneesoul.com>
2020-06-11 23:43:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+function writeCards(recipients, holiday) {
+  let thankCard = []
+  for (let i = 0; i < recipients.length; i++) {
+    thankCard.push(`Thank you, ${recipients[i]}, for the wonderful ${holiday} gift!`);
+  }
+  return thankCard;
+}
+
+function countDown(num) {
+// let count = num;
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
+
